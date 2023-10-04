35ed204c77e5de0350dddd511b771453152bb032
05WilliamCH <wchiroym@miumg.edu.gt>
2023-10-03 21:10:34 -0600

    v1

--- database/pasteleria-israel.sql
+++ database/pasteleria-israel.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE pasteleria_israel
 
 create table usuario(
-	iduser int not null primary key,
-	nombre varchar(30) not null,
-	apellido varchar(40) not null,
-	telefono int not null,
-	email varchar(70) not null,
-	contrasenia varchar(200) not null)
+	iduser int primary key,
+	nombre varchar(30),
+	apellido varchar(40),
+	telefono int,
+	email varchar(70),
+	contrasenia varchar(200))
